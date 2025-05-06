a960252a4ea68f860384e1a611ca019689552a03
BloodReaperJr <shubham.kotgire@gmail.com>
2025-05-06 19:46:17 +0530

    First

---
+++ src/CORE_JAVA/COLLECTIONS/Main.java
@@ -0,0 +1,15 @@
+package CORE_JAVA.COLLECTIONS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> alist = new ArrayList<>();
+        alist.add(1);
+        alist.add(100);
+        alist.add(1000);
+
+        System.out.println(alist);
+    }
+}
